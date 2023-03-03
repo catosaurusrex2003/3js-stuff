785d60bb556c7ebfecbf49fc7643588a21052aef
catosaurusrex2003 <mohdmehdi2003@gmail.com>
2023-03-04 03:16:53 +0530

    super glitchy

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import WebApp1 from "./components/webapp1";
+import WebApp2 from "./components/webapp2";
+import "./App.css"
+import WebApp3 from "./components/webapp3";
+import WebApp4 from "./components/webapp4";
+
+function App() {
+  
+
+  return (
+    <div>
+      <h1>meowadasdasdasdasdasdasdasdasda</h1>
+      <WebApp1/>
+      {/* <WebApp2/> */}
+      <WebApp3/>
+      <WebApp4/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/webapp3.jsx
@@ -0,0 +1,34 @@
+import { useEffect } from 'react';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import SceneInit from '../lib/SceneInit';
+
+function WebApp3() {
+  useEffect(() => {
+    const test = new SceneInit('myThreeJsCanvas');
+    test.initialize();
+    test.animate();
+
+
+    let loadedModel;
+    const glftLoader = new GLTFLoader();
+    glftLoader.load('./assets/myscene3/scene.gltf', (gltfScene) => {
+      gltfScene.scene.rotation.y = Math.PI / 8;
+      gltfScene.scene.position.y = 3;
+      gltfScene.scene.scale.set(10, 10, 10);
+      test.scene.add(gltfScene.scene);
+    });
+
+    const animate = () => {
+      requestAnimationFrame(animate);
+    };
+    animate();
+  }, []);
+
+  return (
+    <div className='canvas-master'>
+      <canvas className='canvas' id="myThreeJsCanvas" />
+    </div>
+  );
+}
+
+export default WebApp3;
